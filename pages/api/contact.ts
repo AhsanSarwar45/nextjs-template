736e4ac24bc6dea1934d73bc6b87199ef28ec289
Ahsan Sarwar <ahsanrockzs@gmail.com>
2022-07-21 22:13:30 +0500

    Add cypress

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -11,8 +11,11 @@ const sendEmail = (req: NextApiRequest, res: NextApiResponse) => {
         to: "azeemsarwarrockzs@gmail.com",
         subject: `Message From ${req.body.name}`,
         text: req.body.message + " | Sent from: " + req.body.email,
-        html: `<div>${req.body.message}</div><p>Sent from:
-        ${req.body.email}</p>`,
+        html: `
+        <div>${req.body.message}</div><p>Sent from:
+        ${req.body.email}</p>
+        `,
+
         onError: (error: any) => {
             res.status(400).send({});
             console.log(error);
