fa988371645a6e2a32c294293a8e2bbf5db074af
Ahsan Sarwar <ahsanrockzs@gmail.com>
2022-07-21 21:17:40 +0500

    Add contact related templates

--- components/page/page.tsx
+++ components/page/page.tsx
@@ -1,46 +1,46 @@
 import { VStack, Box } from "@chakra-ui/react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import Footer from "../footer";
-import Header from "../header";
-import Nav from "../nav";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Nav from "@/components/nav";
 import { PageProps } from "./types";
 import { animation } from "./animations";
 
 const Page = (props: PageProps) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const onMenuOpenScrollPos = useRef(0);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const onMenuOpenScrollPos = useRef(0);
 
-  const setOnMenuOpenScrollPos = (pos: number) => {
-    onMenuOpenScrollPos.current = pos;
-  };
+    const setOnMenuOpenScrollPos = (pos: number) => {
+        onMenuOpenScrollPos.current = pos;
+    };
 
-  useEffect(() => {
-    if (!isMenuOpen) {
-      window.scrollTo(0, onMenuOpenScrollPos.current);
-    }
-  }, [isMenuOpen]);
+    useEffect(() => {
+        if (!isMenuOpen) {
+            window.scrollTo(0, onMenuOpenScrollPos.current);
+        }
+    }, [isMenuOpen]);
 
-  return (
-    <motion.main
-      key={props.title}
-      variants={animation} // Pass the variant object into Framer Motion
-      initial="initial" // Set the initial state to variants.hidden
-      animate="enter" // Animated state to variants.enter
-      exit="exit" // Exit state (used later) to variants.exit
-      transition={{ type: "linear" }} // Set the transition to linear
-    >
-      <Header title={props.title} />
-      <Nav setOnMenuOpenScrollPos={setOnMenuOpenScrollPos} />
-      <VStack display={isMenuOpen ? "none" : "flex"}>
-        {props.children}
-        {/* <Box height="200rem" /> */}
-        <Footer />
-      </VStack>
-    </motion.main>
-  );
+    return (
+        <motion.main
+            key={props.title}
+            variants={animation} // Pass the variant object into Framer Motion
+            initial="initial" // Set the initial state to variants.hidden
+            animate="enter" // Animated state to variants.enter
+            exit="exit" // Exit state (used later) to variants.exit
+            transition={{ type: "linear" }} // Set the transition to linear
+        >
+            <Header title={props.title} />
+            <Nav setOnMenuOpenScrollPos={setOnMenuOpenScrollPos} />
+            <VStack display={isMenuOpen ? "none" : "flex"}>
+                {props.children}
+                {/* <Box height="200rem" /> */}
+                <Footer />
+            </VStack>
+        </motion.main>
+    );
 };
 
 export default Page;
