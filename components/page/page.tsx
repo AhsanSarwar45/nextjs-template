fee272d7618809545054af39afcd1afb971e3502
Ahsan Sarwar <ahsanrockzs@gmail.com>
2022-07-21 22:01:51 +0500

    Merge page changes

--- components/page/page.tsx
+++ components/page/page.tsx
@@ -3,21 +3,13 @@ import { motion } from "framer-motion";
 
 import { useEffect, useRef, useState } from "react";
 
-<<<<<<< HEAD
-import Footer from "../footer";
-import Header from "../header/header";
-import Nav from "../nav/nav";
-=======
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Nav from "@/components/nav";
->>>>>>> a51549b376a39808f8e6df1f04a34778710f9e20
 import { PageProps } from "./types";
 import { animation } from "./animations";
 
 const Page = (props: PageProps) => {
-<<<<<<< HEAD
-=======
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const onMenuOpenScrollPos = useRef(0);
 
@@ -31,7 +23,6 @@ const Page = (props: PageProps) => {
         }
     }, [isMenuOpen]);
 
->>>>>>> a51549b376a39808f8e6df1f04a34778710f9e20
     return (
         <motion.main
             key={props.title}
@@ -42,13 +33,8 @@ const Page = (props: PageProps) => {
             transition={{ type: "linear" }} // Set the transition to linear
         >
             <Header title={props.title} />
-<<<<<<< HEAD
             <Nav />
-            <VStack>
-=======
-            <Nav setOnMenuOpenScrollPos={setOnMenuOpenScrollPos} />
             <VStack display={isMenuOpen ? "none" : "flex"}>
->>>>>>> a51549b376a39808f8e6df1f04a34778710f9e20
                 {props.children}
                 {/* <Box height="200rem" /> */}
                 <Footer />
